e799bd98877ac71b6f4747410fc97c12d6a12488
AbuQamar Shaikh <shaikhqamar7379@gmail.com>
2024-03-21 03:39:42 +0530

    backend deployed

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -7,5 +7,10 @@ const commentRouter = require("./comments.routes");
 router.use("/auth", authRouter);
 router.use("/blog", blogRouter);
 router.use("/comment", commentRouter);
+router.get("/", (req, res) =>
+  res
+    .status(200)
+    .send("<h2>Blogging Platform Application server is runnig.......</h2>")
+);
 
 module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PostBlog from "./components/PostBlog/PostBlog";
 
 export const config = {
-  endpoint: "http://localhost:2000",
+  endpoint: "https://blogging-platform-application.onrender.com/",
 };
 
 function App() {
